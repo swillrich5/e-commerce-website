5d16e2df174eed8deb43aab580dd3f11fd0ab79b
swillrich5 <swillrich@gmail.com>
2021-04-27 17:07:54 -0500

    sequelize table definitions complete, tables seeded

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: "product_tag",
+  as: "tag",
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: "product_tag",
+  as: "tag",
+  foreignKey: "tag_id",
+});
+
 
 module.exports = {
   Product,
